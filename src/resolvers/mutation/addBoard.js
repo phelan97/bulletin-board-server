4e1ea1cef957fd47469dd1a1746d5c292b5dd9e2
Sean Phelan <seanphelan97@gmail.com>
2019-01-03 22:39:49 -0700

    move git file and updated addCard resolver

--- src/resolvers/mutation/addBoard.js
+++ src/resolvers/mutation/addBoard.js
@@ -2,7 +2,6 @@ const Board = require('../../models/board');
 
 module.exports = async (root, args, context) => {
   const decodedToken = context.isAuthorized();
-  console.log(decodedToken.user.id);
   const board = await Board.create({userId: decodedToken.user.id, title: args.title});
   return board.toObject();
 }
--- src/resolvers/mutation/addCard.js
+++ src/resolvers/mutation/addCard.js
@@ -2,5 +2,6 @@ const Card = require('../../models/card');
 
 module.exports = async (root, args, context) => {
   const decodedToken = context.isAuthorized();
-
+  const card = await Card.create({userId: decodedToken.user.id, listId: args.listId,  content: args.content});
+  return card.toObject();
 }
