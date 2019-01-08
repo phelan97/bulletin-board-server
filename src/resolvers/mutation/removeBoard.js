337fc573140ec5f44c43bb6f2383c3f249f5f870
Sean Phelan <seanphelan97@gmail.com>
2019-01-08 13:09:14 -0700

    add wip id return to all remove mutatins

--- src/resolvers/mutation/removeBoard.js
+++ src/resolvers/mutation/removeBoard.js
@@ -3,4 +3,5 @@ const Board = require('../../models/board');
 module.exports = async (root, args, context) => {
   const decodedToken = context.isAuthorized();
   await Board.deleteOne({userId: decodedToken.user.id, _id: args.boardId});
+  return args.boardId;
 }
--- src/resolvers/mutation/removeList.js
+++ src/resolvers/mutation/removeList.js
@@ -3,4 +3,5 @@ const List = require('../../models/board');
 module.exports = async (root, args, context) => {
   const decodedToken = context.isAuthorized();
   await List.deleteOne({userId: decodedToken.user.id, _id: args.listId});
+  return args.listId;
 }
