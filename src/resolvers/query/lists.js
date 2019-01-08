308fa0ae5c33d6796624b03f5a12fe5e9b4239c3
Sean Phelan <seanphelan97@gmail.com>
2019-01-07 20:03:34 -0700

    remove console logs

--- src/resolvers/query/lists.js
+++ src/resolvers/query/lists.js
@@ -3,15 +3,8 @@ const mongoose = require('mongoose');
 
 module.exports = async (root, args, context) => {
   const decodedToken = context.isAuthorized();
-  console.log(decodedToken.user.id);
-  console.log(args.boardId);
   const userId = mongoose.Types.ObjectId(decodedToken.user.id);
   const boardId = mongoose.Types.ObjectId(args.boardId);
-  const userLists = await List.find({userId});
-  console.log(userLists);
-  const listFromBoard = await List.find({boardId});
-  //console.log(listFromBoard);
   const lists = await List.find({userId, boardId: boardId});
-  console.log(lists);
   return lists;
 }
