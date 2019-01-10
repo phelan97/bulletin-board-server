cc47223779aedcfc75c23b466e9231ae525ea3a1
Sean Phelan <seanphelan97@gmail.com>
2019-01-10 00:55:00 -0700

    automatically create a demo account if it does not exist

--- src/resolvers/query/demoAccount.js
+++ src/resolvers/query/demoAccount.js
@@ -4,16 +4,20 @@ const bcrypt = require('bcryptjs');
 
 module.exports = async (root, args) => {
   const {DEMO_EMAIL: email, DEMO_PASSWORD: password} = process.env;
-  
+  if(!email || !password) {
+    throw('Could not log into the demo account. Server variables are not set');
+  }
   const user = await User.findOne({email});
   if(!user) {
-    // username doesn't match
-    throw('Could not log in to the demo account. Check server config and db');
+    // create demo account if it doesn't already exist
+    const hash = await bcrypt.hash(password, 10);
+    const user = await User.create({email, password: hash});
+    return jwt.sign({user}, process.env.JWT_SECRET);
   }
   const isMatchingPassword = await bcrypt.compare(password, user.password);
   if(!isMatchingPassword) {
     // invalid password for the given user
-    throw('Could not log in to the demo account. Check server config and db');
+    throw('Invalid credentials');
   }
   return jwt.sign({user}, process.env.JWT_SECRET);
 }
